21c8392a8cd381fe9c992ac64dfee6a5929e35ae
jason <kwon_jason@yahoo.com>
2021-09-07 20:27:40 +1000

    update v2 fix

--- services/collectiontracker.js
+++ services/collectiontracker.js
@@ -3,20 +3,10 @@ const ethers = require('ethers')
 
 const { default: axios } = require('axios')
 
-const mongoose = require('mongoose')
-const NFTITEM = mongoose.model('NFTITEM')
-const Category = mongoose.model('Category')
-const ERC721CONTRACT = mongoose.model('ERC721CONTRACT')
-
-const contractutils = require('./contract.utils')
-
 const rpcapi = process.env.NETWORK_RPC
 const chainID = parseInt(process.env.NETWORK_CHAINID)
 const provider = new ethers.providers.JsonRpcProvider(rpcapi, chainID)
-const provider1 = new ethers.providers.JsonRpcProvider(
-  process.env.NETWORK_RPC1,
-  chainID,
-)
+
 const toLowerCase = (val) => {
   if (val) return val.toLowerCase()
   else return val
@@ -31,41 +21,18 @@ const parseTokenID = (hexData) => {
   return parseInt(hexData.toString())
 }
 
-const getBlockTime = async (blockNumber) => {
-  let block = await provider1.getBlock(parseInt(blockNumber))
-  let blockTime = block.timestamp
-  blockTime = new Date(blockTime * 1000)
-  return blockTime
-}
-const bannedCollections = new Map()
+const apiEndPoint = process.env.API_ENDPOINT
 
-const isBannedCollection = async (contractAddress) => {
-  let isBanned = bannedCollections.get(contractAddress)
-  if (isBanned) return true
-  try {
-    let contract_721 = await ERC721CONTRACT.findOne({
-      address: contractAddress,
-      isAppropriate: false,
-    })
-    if (contract_721) {
-      bannedCollections.set(contractAddress, true)
-      return true
-    } else {
-      bannedCollections.set(contractAddress, false)
-      return false
-    }
-  } catch (error) {
-    return false
-  }
+const callAPI = async (endpoint, data) => {
+  console.log(data)
+  await axios({
+    method: 'post',
+    url: apiEndPoint + endpoint,
+    data,
+  })
 }
 
-const trackSingleContract = async (sc, address) => {
-  try {
-    let category = new Category()
-    category.minterAddress = contractInfo.address
-    category.type = 721
-    await category.save()
-  } catch (error) {}
+const trackSingleContract = async (address) => {
   let eventLogs = await provider.getLogs({
     address: address,
     fromBlock: 0,
@@ -79,89 +46,29 @@ const trackSingleContract = async (sc, address) => {
 
   let tokenIDs = []
   let ownerMap = new Map()
-  let blockNumberMap = new Map()
 
   eventLogs.map((eventLog) => {
     let topics = eventLog.topics
     let receiver = toLowerCase(extractAddress(topics[2]))
     let tokenID = parseTokenID(topics[3])
-    blockNumberMap.set(tokenID, eventLog.blockNumber)
     ownerMap.set(tokenID, receiver)
     if (!tokenIDs.includes(tokenID)) tokenIDs.push(tokenID)
   })
 
   let promise = tokenIDs.map(async (tokenID, index) => {
     setTimeout(async () => {
-      try {
-        let erc721token = await NFTITEM.findOne({
-          contractAddress: address,
-          tokenID: tokenID,
-        })
-        if (erc721token) {
-          if (erc721token.owner != ownerMap.get(tokenID)) {
-            erc721token.owner = ownerMap.get(tokenID)
-            await erc721token.save()
-          }
-        } else {
-          // return
-          let tokenURI = await sc.tokenURI(tokenID)
-          if (tokenURI.startsWith('https://')) {
-            // if (tokenURI.endsWith('CqT'))
-            try {
-              let newTk = new NFTITEM()
-              newTk.contractAddress = address
-              newTk.tokenID = tokenID
-              newTk.tokenURI = tokenURI
-              newTk.owner = ownerMap.get(tokenID)
-              let tokenName = ''
-              let imageURL = ''
-              try {
-                let metadata = await axios.get(tokenURI)
-                if (metadata) {
-                  tokenName = metadata.data.name
-                  imageURL = metadata.data.image
-                }
-              } catch (error) {
-                console.log(error)
-              }
-              newTk.name = tokenName
-              newTk.imageURL = imageURL
-              try {
-                let mintTime = await getBlockTime(blockNumberMap.get(tokenID))
-                newTk.createdAt = mintTime
-              } catch (error) {
-                console.log(error)
-              }
-              try {
-                let isBanned = await isBannedCollection(address)
-                newTk.isAppropriate = !isBanned
-                await newTk.save()
-              } catch (error) {
-                console.log(error)
-              }
-            } catch (error) {
-              console.log(error)
-            }
-          }
-        }
-      } catch (error) {}
+      let to = ownerMap.get(tokenID)
+      await callAPI('handle721Transfer', { address, to, tokenID })
     }, index * 100)
   })
 
   await Promise.all(promise)
 }
 
-const trackERC721Distribution = (contracts) => {
-  let promise = contracts.map((contract) => {
-    let sc = contractutils.loadContractFromAddress(contract.address)
-    trackSingleContract(sc, contract.address)
-    // await trackSingleContract(sc, contract.address)
+const trackERC721Distribution = (addresses) => {
+  addresses.map((address) => {
+    trackSingleContract(address)
   })
-  // await Promise.all(promise)
 }
 
-const collectionTracker = {
-  trackERC721Distribution,
-}
-
-module.exports = collectionTracker
+module.exports = trackERC721Distribution
--- services/erc721tracker.js
+++ services/erc721tracker.js
@@ -1,91 +1,47 @@
 require('dotenv').config()
 const axios = require('axios')
-const ethers = require('ethers')
 
-const rpcapi = process.env.NETWORK_RPC
-const chaindID = parseInt(process.env.NETWORK_CHAINID)
-
-const provider = new ethers.providers.JsonRpcProvider(rpcapi, chaindID)
-
-const collectionTracker = require('./collectiontracker')
+const trackERC721Distribution = require('./collectiontracker')
 const contractutils = require('./contract.utils')
-
-const ftmScanApiKey = process.env.FTM_SCAN_API_KEY
-const validatorAddress = process.env.VALIDATORADDRESS
-const ftmScanURL = process.env.FTM_SCAN_URL
-const limit = 99999999999
-
-const toLowerCase = (val) => {
-  if (val) return val.toLowerCase()
-  else return val
-}
+const apiEndPoint = process.env.API_ENDPOINT
 
 const trackedAddresses = []
 const trackedContracts = []
 
-const trackerc721 = async (begin, end) => {
+const trackerc721 = async () => {
+  let contracts = []
   try {
-    let contracts = new Array()
-
-    let request = `${ftmScanURL}api?module=account&action=tokennfttx&address=${validatorAddress}&startblock=${begin}&endblock=${end}&sort=asc&apikey=${ftmScanApiKey}`
-    let result = await axios.get(request)
-    let tnxs = result.data.result
-    if (tnxs) {
-      let last = tnxs[tnxs.length - 1]
-      end = parseInt(last.blockNumber)
-    }
-
-    if (tnxs.length == 0) return end
-    if (tnxs) {
-      let promises = tnxs.map(async (tnx) => {
-        let contractInfo = {
-          address: toLowerCase(tnx.contractAddress),
-          name: tnx.tokenName,
-          symbol: tnx.tokenSymbol,
-        }
-        if (
-          !contracts.some(
-            (contract) => contract.address == contractInfo.address,
-          )
-        ) {
-          if (!trackedAddresses.includes(contractInfo.address)) {
-            contracts.push(contractInfo)
-            let sc = contractutils.loadContractFromAddress(contractInfo.address)
-            trackedAddresses.push(contractInfo.address)
-            console.log(contractInfo.address)
+    let response = await axios.get(`${apiEndPoint}getTrackable721Contracts`)
+    if (response) {
+      let data = response.data
+      if (data.status == 'success') {
+        data = data.data
+        data.map((address) => {
+          if (!trackedAddresses.includes(address)) {
+            let sc = contractutils.loadContractFromAddress(address)
+            trackedAddresses.push(address)
             trackedContracts.push(sc)
+            contracts.push(address)
           }
-        }
-      })
-      await Promise.all(promises)
-      // console.log(trackedAddresses)
-      await collectionTracker.trackERC721Distribution(contracts)
+        })
+        trackERC721Distribution(contracts)
+      }
     }
-    return end
   } catch (error) {
     console.log(error)
   }
 }
 
-let start = 1
-
 const trackAll721s = async () => {
   const func = async () => {
     try {
-      let currentBlockHeight = await provider.getBlockNumber()
-      start = await trackerc721(start, currentBlockHeight)
-      if (currentBlockHeight > limit) start = 0
+      await trackerc721()
       setTimeout(async () => {
         await func()
-      }, 1000 * 60)
+      }, 1000 * 60 * 10 * 3)
     } catch (error) {}
   }
   await func()
 }
 
-const Tracker = {
-  trackedContracts,
-  trackAll721s,
-}
-
-module.exports = Tracker
+module.exports = trackAll721s
