214c3571f67c61ed8353d11f900741822ca70b67
jason <kwon_jason@yahoo.com>
2021-09-07 20:27:40 +1000

    update v2 fix

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
 
-require('./models/nftitems')
-require('./models/category')
-require('./models/erc721contract')
+const trackAll721s = require('./services/erc721tracker')
 
-const Tracker = require('./services/erc721tracker')
-
-const uri = process.env.DB_URL
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', async () => {
-  console.log('721 tracker has been connected to the db server')
-  Tracker.trackAll721s()
-})
+trackAll721s()
