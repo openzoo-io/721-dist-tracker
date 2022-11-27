f5b3b1f9a4bfcbbadbfd17a888c07e49f951e9b3
lolieatapple <lolieatapple@gmail.com>
2022-11-27 20:51:53 +0800

    add db records

--- services/erc721tracker.js
+++ services/erc721tracker.js
@@ -1,12 +1,27 @@
 require('dotenv').config()
 const axios = require('axios')
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const trackERC721Distribution = require('./collectiontracker')
 const contractutils = require('./contract.utils')
 const apiEndPoint = process.env.API_ENDPOINT
+const dbURL = process.env.DB_URL
+let dbClient = null;
+let dbCollection = null;
 
-const trackedAddresses = []
+let trackedAddresses = []
 const trackedContracts = []
+
+const readTrackedFromApi = async () => {
+  const response = await axios.get(apiEndPoint + '/tracked')
+  const tracked = response.data
+  tracked.forEach((t) => {
+    if (t.type === 'address') {
+      trackedAddresses.push(t.address)
+    } else if (t.type === 'contract') {
+      trackedContracts.push(t.address)
+    }
+  })
+
 
 const trackerc721 = async () => {
   let contracts = []
@@ -22,6 +37,7 @@ const trackerc721 = async () => {
             trackedAddresses.push(address)
             trackedContracts.push(sc)
             contracts.push(address)
+            await dbCollection.insertOne({ address: address, type: 'erc721' });
           }
         })
         console.info(`Tracking ${contracts.length} new contracts`);
@@ -36,6 +52,18 @@ const trackerc721 = async () => {
 const trackAll721s = async () => {
   const func = async () => {
     try {
+      dbClient = await MongoClient.connect(dbURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: {
+          version: ServerApiVersion.v1,
+        },
+      });
+      console.log('connect db success');
+      dbCollection = dbClient.db('test').collection('tracked721');
+      const tracked = await dbCollection.find({}).toArray();
+      trackedAddresses = tracked.map((t) => t.address);
+      console.log('trackedAddresses', trackedAddresses);
       await trackerc721()
       setTimeout(async () => {
         await func()
